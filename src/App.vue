f35a0da4c2260b78568a25022314285edf224636
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-03-10 20:47:50 +0300

    добавление навигации

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <mcv-topbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar.vue'
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar,
+  },
+}
+</script>
